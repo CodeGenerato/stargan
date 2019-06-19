aa8f16bdbeae7e35d6c8272f6360814e3d27ef52
Nico <Nico@mail>
2019-06-19 20:52:04 +0200

    clean up, refactoring

--- data_loader.py
+++ data_loader.py
@@ -7,14 +7,12 @@ import os
 import random
 
 
-class CelebA(data.Dataset):
+class CarDataset(data.Dataset):
     """Dataset class for the CelebA dataset."""
 
-    def __init__(self, image_dir, attr_path, selected_attrs, transform, mode):
+    def __init__(self, image_dir, transform, mode):
         """Initialize and preprocess the CelebA dataset."""
         self.image_dir = image_dir
-        self.attr_path = attr_path
-        self.selected_attrs = selected_attrs
         self.transform = transform
         self.mode = mode
         self.train_dataset = []
@@ -30,28 +28,21 @@ class CelebA(data.Dataset):
 
     def preprocess(self):
         """Preprocess the CelebA attribute file."""
-        # lines = [line.rstrip() for line in open(self.attr_path, 'r')]
-        # all_attr_names = lines[1].split()
-        # for i, attr_name in enumerate(all_attr_names):
-        #     self.attr2idx[attr_name] = i
-        #     self.idx2attr[i] = attr_name
-
-        # read all file names, shuffle and create filename, label pairs
+      
         file_name_list = os.listdir(self.image_dir)
         random.seed(1234)
         random.shuffle(file_name_list)
 
         
-        #112 ferrari x
-        #117 maserati y
-        #139 ??     y
-        #141 lotus -
-        #142 landrover y
-        #57 RR
-        #78 audi
-        #95 skoda
-        #77 merceds y
-        #81 bmw y
+        # 112 ferrari 
+        # 117 maserati 
+        # 141 lotus 
+        # 142 landrover 
+        # 57 RR
+        # 78 audi
+        # 95 skoda
+        # 77 merceds 
+        # 81 bmw 
         self.attr2idx[77]=0
         self.attr2idx[81]=1
         self.attr2idx[78]=2
@@ -59,7 +50,7 @@ class CelebA(data.Dataset):
         self.attr2idx[111]=4
 
         for i, file_name in enumerate(file_name_list):
-            if file_name.startswith('X_'):
+            if (file_name.startswith('X_')):
                 continue
             
             parts = file_name.split("-")
@@ -70,18 +61,6 @@ class CelebA(data.Dataset):
            
             self.train_dataset.append([img_name, self.attr2idx[label]])
 
-        # lines = lines[2:]
-
-        # for i, line in enumerate(lines):
-        #     split = line.split()
-        #     filename = split[0]
-        #     values = split[1:]
-
-        #     label = []
-        #     for attr_name in self.selected_attrs:
-        #         idx = self.attr2idx[attr_name]
-        #         label.append(values[idx] == '1')
-
         print('Finished preprocessing the CelebA dataset...')
 
     def __getitem__(self, index):
@@ -89,18 +68,19 @@ class CelebA(data.Dataset):
         dataset = self.train_dataset if self.mode == 'train' else self.test_dataset
         filename, label = dataset[index]
         image = Image.open(os.path.join(self.image_dir, filename))
-
-        a=torch.zeros(5, dtype=torch.float32)
-        a[label]=1
-        return self.transform(image), a
+        
+        #TODO magic num
+        encoded_lab=torch.zeros(5, dtype=torch.float32)
+        encoded_lab[label]=1
+        return self.transform(image), encoded_lab
 
     def __len__(self):
         """Return the number of images."""
         return self.num_images
 
 
-def get_loader(image_dir, attr_path, selected_attrs, crop_size=178, image_size=128,
-               batch_size=16, dataset='CelebA', mode='train', num_workers=1):
+def get_loader(image_dir, crop_size=178, image_size=128,
+               batch_size=16, mode='train', num_workers=1):
     """Build and return a data loader."""
     transform = []
     if mode == 'train':
@@ -111,11 +91,7 @@ def get_loader(image_dir, attr_path, selected_attrs, crop_size=178, image_size=1
     transform.append(T.Normalize(mean=(0.5, 0.5, 0.5), std=(0.5, 0.5, 0.5)))
     transform = T.Compose(transform)
 
-    if dataset == 'CelebA':
-        dataset = CelebA(image_dir, attr_path, selected_attrs, transform, mode)
-    elif dataset == 'RaFD':
-        raise AttributeError("RaFD not supported")
-
+    dataset = CarDataset(image_dir, transform, mode)
     data_loader = data.DataLoader(dataset=dataset,
                                   batch_size=batch_size,
                                   shuffle=(mode == 'train'),
