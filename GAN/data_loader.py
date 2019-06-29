38578fac903524e568f3c93ae18af21fc44932b7
Nico <Nico@mail>
2019-06-29 11:43:38 +0200

    new data aug setting

--- GAN/data_loader.py
+++ GAN/data_loader.py
@@ -130,8 +130,11 @@ def get_loader(domains,image_dir, crop_size=178, image_size=128,
     transform = []
     
     #data Augmentation with required transformations
+    # # transform.extend([T.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.3, hue=0.5), T.RandomHorizontalFlip(), T.RandomAffine(degrees = 2)])
+
     if mode == 'train':
-        transform.extend([T.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.3, hue=0.5), T.RandomHorizontalFlip(), T.RandomAffine(degrees = 2)])
+       transform.extend([T.ColorJitter(brightness=0.2, contrast=0.1, saturation=0.2, hue=0.1), T.RandomHorizontalFlip(), T.RandomAffine(degrees = 2)])
+    
     # transform.append(T.CenterCrop(crop_size))
     transform.append(T.Resize(image_size))
     transform.append(T.ToTensor())
