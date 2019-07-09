1d27cd398c6fcccffb4fb8ba6e715874608f5ed6
shadowwalkers <anurag4446@mail.com>
2019-07-09 15:43:22 +0200

    update evaluate model

--- GAN/evaluation/evaluateModel.py
+++ GAN/evaluation/evaluateModel.py
@@ -9,6 +9,35 @@ import scipy.io
 #conda install -c conda-forge caffe
 # run in python
 
+def transformframe(frame) :
+    # bring to range [0,1]
+    frame = frame / 255.0
+    # normalise with assignment mean and std dev
+    frame[:,:,0] = (frame[:,:,0] - 0.5) / 0.5
+    frame[:,:,1] = (frame[:,:,1] - 0.5) / 0.5
+    frame[:,:,2] = (frame[:,:,2] - 0.5) / 0.5
+    
+    return frame
+
+def classify(imageloc, net, mat) :
+    frame = cv2.imread(imageloc)
+    u1 = frame[:,:,0]
+    u2 = frame[:,:,1]
+    u3 = frame[:,:,2]
+    
+    v1 = np.sum(u1)/(256 * 256)
+    v2 = np.sum(u2)/(256*256)
+    v3 = np.sum(u3)/(256*256)
+
+    blob = cv2.dnn.blobFromImage(cv2.resize(frame, (224, 224)), 1, (224, 224), (v1, v2, v3))
+    net.setInput(blob)
+    detections = net.forward()
+
+    res = np.where(detections[0] == np.amax(detections[0]))
+    print("confidence " + str(np.amax(detections[0])) + " make " + str(mat['make_model_names'][res]))
+
+
+
 ap = argparse.ArgumentParser()
 ap.add_argument("-p", "--prototxt", default = 'deploy.prototxt',
 	help="path to Caffe 'deploy' prototxt file")
@@ -20,14 +49,4 @@ mat = scipy.io.loadmat('make_model_names_cls.mat')
 
 # take the model
 net = cv2.dnn.readNetFromCaffe(args["prototxt"], args["model"])
-
-frame = cv2.imread("m.jpg")
-blob = cv2.dnn.blobFromImage(cv2.resize(frame, (224, 224)),
-	1, (224, 224))
-
-#feed input
-net.setInput(blob)
-detections = net.forward()
-
-res = np.where(detections[0] == np.amax(detections[0]))
-print("confidence " + str(np.amax(detections[0])) + " make " + str(mat['make_model_names'][res]))
+classify("m.jpg", net, mat)
