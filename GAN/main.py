883b44c90c0093e042ac5e755d6f3aeb2ef17e6b
Nico <Nico@mail>
2019-07-10 14:34:21 +0200

    2 domains

--- GAN/main.py
+++ GAN/main.py
@@ -75,9 +75,9 @@ if __name__ == '__main__':
     # 73 vw
     
     parser.add_argument('--domains', '--list', nargs='+', help='domains of car data set',
-                        default=[77,81,78,95,158,111,157,73]) 
+                        default=[77,81]) 
 
-    parser.add_argument('--c_dim', type=int, default=8, help='dimension of domain labels (1st dataset)')
+    parser.add_argument('--c_dim', type=int, default=2, help='dimension of domain labels (1st dataset)')
     parser.add_argument('--crop_size', type=int, default=128, help='crop size dataset')
     parser.add_argument('--image_size', type=int, default=128, help='image resolution')
     parser.add_argument('--g_conv_dim', type=int, default=64, help='number of conv filters in the first layer of G')
